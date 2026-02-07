619d441c69b02a5927b25407f92cd9a77f49510f
mertso13 <mertsagiroglu2004@gmail.com>
2026-02-07 13:50:23 +0300

    feat(dependencies): add spring-boot-starter-web dependency for web functionalities

--- src/main/java/io/github/mertso13/taskmanagerapi/TaskmanagerapiApplication.java
+++ src/main/java/io/github/mertso13/taskmanagerapi/TaskmanagerapiApplication.java
@@ -2,8 +2,10 @@ package io.github.mertso13.taskmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class TaskmanagerapiApplication {
 
 	public static void main(String[] args) {
